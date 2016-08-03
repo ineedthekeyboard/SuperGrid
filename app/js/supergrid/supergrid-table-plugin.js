f99702aceb2aa7c7b31ab35afe3c253ba1d8c3d7
Ryan Carlton <ryan.lee.carlton@gmail.com>
2016-08-03 18:39:55 -0400

    Removed Table....Refactoring building of table. Experimental commit. Sizing of grid containers needs fixed.

---
+++ app/js/supergrid/supergrid-table-plugin.js
@@ -0,0 +1,155 @@
+$.widget('custom.SuperGrid-table', 'custom.SuperGrid' {
+
+    _bindListeners: function() {
+        var context = this;
+
+        this.element.off('click', '.supergrid_header td[data-sortable="true"]');
+        this.element.on('click', '.supergrid_header td[data-sortable="true"]', function(e) {
+            var $elem = $(this),
+                currSort = $elem.attr('data-sort'),
+                id = $elem.attr('data-id'),
+                columns = context.options.columns,
+                newSort;
+
+            context.element.find('.supergrid_header td').removeAttr('data-sort');
+
+            switch (currSort) {
+                case 'asc':
+                    newSort = 'desc';
+                    break;
+                case 'desc':
+                default:
+                    newSort = 'asc';
+            }
+            $.each(columns, function(i, col) {
+                if (col.id === id) {
+                    col.sort = newSort;
+                    return true;
+                }
+                delete col.sort;
+            });
+
+            $elem.attr('data-sort', newSort);
+            context._trigger('-sorted', e, {
+                columns: context.options.columns
+            });
+            context._renderGrid();
+        });
+
+    },
+
+    /**
+     * @memberOf SuperGrid
+     * @description Render data and columns
+     * @private
+     */
+    _renderGrid: function() {
+        this.element.html(this._buildGrid());
+    },
+
+    _buildGrid: function() {
+        var headerHtml = this._buildHeader(),
+            bodyHtml = this._buildBody(),
+            html = '<table class="supergrid" style="width:100%;table-layout: fixed;">';
+
+        /* Make header its own table */
+        if (this.options.fixedHeader) {
+            html = '<table class="supergrid supergrid-fheader">' + headerHtml + '</table>' +
+                '<div class="supergrid-body">' + html;
+        } else {
+            html += headerHtml;
+        }
+
+        html += '<tbody>';
+        html += bodyHtml;
+        html += '</tbody>';
+        html += (this.options.fixedHeader) ? '</table></div>' : '</table>';
+        return html;
+    },
+
+    _buildHeader: function() {
+        var headerHtml = '<thead class="supergrid_header">';
+        headerHtml += '<tr>';
+        $.each(this.options.columns, function(i, col) {
+            var cellClass = col.cellClass || '',
+                width = col.width || '',
+                id = col.id || '',
+                name = col.name || '',
+                sort = col.sort || '',
+                sortable = col.sortable || '';
+
+            headerHtml += '<td style="width:' + col.width + 'px;" scope="col" class="' + cellClass + '" data-id="' + id + '" tabIndex="0"';
+            if (sort) {
+                headerHtml += 'data-sort="' + sort + '" ';
+            }
+            headerHtml += 'data-sortable="' + sortable + '">';
+            headerHtml += '<div>';
+            headerHtml += name;
+            if (sort) {
+                headerHtml += '<div class="sort-icon"></div>';
+            }
+            headerHtml += '</div>';
+            headerHtml += '</td>';
+        });
+        headerHtml += '</tr>';
+        headerHtml += '</thead>';
+        return headerHtml;
+    },
+
+    _buildBody: function() {
+        var data = this.options.data,
+            columns = this.options.columns,
+            context = this,
+            bodyHtml = '';
+        $.each(data, function(i, dataSet) {
+            var id = dataSet.id || '';
+
+            bodyHtml += '<tr class="section" data-id="' + id + '">';
+            $.each(columns, function(i, col) {
+                var cellClass = col.cellClass || '';
+                bodyHtml += '<td style="width:' + col.width + 'px; class="' + cellClass + '" tabIndex="0">';
+                bodyHtml += '<div>';
+                bodyHtml += context._buildCell(dataSet, col);
+                bodyHtml += '</div>';
+                bodyHtml += '</td>';
+            });
+            bodyHtml += '</tr>';
+        });
+        return bodyHtml;
+    },
+
+    _buildCell: function(data, column) {
+        var attrs = [],
+            regex = /\#(.*?)\#/,
+            formatter = column.formatter,
+            formatterHelper = formatter,
+            matchedAttr = regex.exec(formatter);
+        if (!data) {
+            return '';
+        }
+        if (typeof formatter === 'function') {
+            return formatter(data);
+        }
+        if (typeof formatter === 'object') {
+            formatter = column.formatter;
+        }
+
+        formatterHelper = formatter;
+        matchedAttr = regex.exec(formatter);
+        if (formatterHelper) {
+            while (matchedAttr) {
+                attrs.push(matchedAttr[0]);
+                formatterHelper = formatterHelper.replace(matchedAttr[0], '');
+                matchedAttr = regex.exec(formatterHelper);
+            }
+            $.each(attrs, function(index, attr) {
+                var value = attr.replace(/#|_/g, '');
+                formatter = formatter.replace(attr, data[value]);
+            });
+            return formatter;
+        } else {
+            return data[column.id];
+        }
+    }
+
+});
--- app/js/supergrid/supergrid.js
+++ app/js/supergrid/supergrid.js
@@ -1,39 +1,58 @@
+/**
+ *@SuperGrid
+ * @version 0.1
+ * @namespace custom.SuperGrid
+ * @description A feature rich, developer friendly grid for client side javascript.
+ */
 $.widget('custom.SuperGrid', {
+    /**
+     * @name custom.SuperGrid#fixedHeader
+     * @description Defines if the header should scroll or be fixed.
+     * @type {boolean}
+     * @defaultvalue false
+     */
+    /**
+     * @name custom.SuperGrid#paginate
+     * @description Defines if pagination should be enabled.
+     * @type {boolean}
+     * @defaultvalue false
+     */
     options: {
         fixedHeader: false,
         paginate: false
     },
-
     /**
-     * @memberOf SuperGrid
+     * @name custom.SuperGrid#create
+     * @function
+     * @constructor
      * @description constructor, only called once. starts the life cycle of the gadget
+     * @fires custom.SuperGrid#_renderGrid
+     * @fires custom.SuperGrid#_bindListeners
      * @private
      */
-    _create: function () {
+    _create: function() {
         // default column width
-        $.each(this.options.columns, function (index, column) {
+        $.each(this.options.columns, function(index, column) {
             if (!column.width) {
                 column.width = 26;
             }
         });
-
+        this.options._grid = [];
         this._renderGrid();
         this._bindListeners();
     },
 
-
-    _bindListeners: function () {
+    _bindListeners: function() {
         var context = this;
-
-        this.element.off('click', '.supergrid_header td[data-sortable="true"]');
-        this.element.on('click', '.supergrid_header td[data-sortable="true"]', function (e) {
+        this.element.off('click', '.supergrid_header .supergrid_cell[data-sortable="true"]');
+        this.element.on('click', '.supergrid_header .supergrid_cell[data-sortable="true"]', function(e) {
             var $elem = $(this),
                 currSort = $elem.attr('data-sort'),
                 id = $elem.attr('data-id'),
                 columns = context.options.columns,
                 newSort;
 
-            context.element.find('.supergrid_header td').removeAttr('data-sort');
+            context.element.find('.supergrid_header .supergrid_row').removeAttr('data-sort');
 
             switch (currSort) {
                 case 'asc':
@@ -43,7 +62,7 @@ $.widget('custom.SuperGrid', {
                 default:
                     newSort = 'asc';
             }
-            $.each(columns, function (i, col) {
+            $.each(columns, function(i, col) {
                 if (col.id === id) {
                     col.sort = newSort;
                     return true;
@@ -52,32 +71,34 @@ $.widget('custom.SuperGrid', {
             });
 
             $elem.attr('data-sort', newSort);
-            context._trigger('-sorted', e, {columns: context.options.columns});
+            context._trigger('-sorted', e, {
+                columns: context.options.columns
+            });
             context._renderGrid();
         });
-
     },
-
     /**
-     * @memberOf SuperGrid
+     * @name custom.SuperGrid#_renderGrid
      * @description Render data and columns
      * @private
+     * @function
      */
-    _renderGrid: function () {
+    _renderGrid: function() {
         this._sortData();
-        this.element.html(this._buildGrid());
+        this._buildGrid();
+        this.element.html(this.options._grid.join(''));
         this._addMetaData();
         this._trigger('-rendered');
     },
 
-    _addMetaData: function () {
+    _addMetaData: function() {
         var context = this;
-        $.each(this.options.data, function (i, row) {
-            context.element.find('tr[data-id="' + row.id + '"]').data(row);
+        $.each(this.options.data, function(i, row) {
+            context.element.find('supergrid_row[data-id="' + row.id + '"]').data(row);
         });
     },
 
-    _sortData: function () {
+    _sortData: function() {
         var sortObj = this._getSorting(),
             blnAsc,
             getSortValue,
@@ -88,7 +109,7 @@ $.widget('custom.SuperGrid', {
         }
         blnAsc = sortObj.sort === 'asc';
         field = sortObj.field;
-        $.each(this.options.columns, function (i, col) {
+        $.each(this.options.columns, function(i, col) {
             if (col.id === field) {
                 getSortValue = col.getSortValue;
                 customSort = col.sortFunc;
@@ -96,7 +117,7 @@ $.widget('custom.SuperGrid', {
             }
         });
 
-        this.options.data.sort(function (a, b) {
+        this.options.data.sort(function(a, b) {
             if (customSort) {
                 return customSort(a, b, blnAsc);
             }
@@ -111,9 +132,10 @@ $.widget('custom.SuperGrid', {
             return 0;
         });
     },
-    _getSorting: function () {
+
+    _getSorting: function() {
         var sortObj = {};
-        $.each(this.options.columns, function (i, col) {
+        $.each(this.options.columns, function(i, col) {
             if (col.sort) {
                 sortObj.field = col.id;
                 sortObj.sort = col.sort;
@@ -123,122 +145,123 @@ $.widget('custom.SuperGrid', {
         return sortObj;
     },
 
-    _buildGrid: function () {
-        var headerHtml = this._buildHeader(),
-            bodyHtml = this._buildBody(),
-            html = '<table class="supergrid" style="width:100%;table-layout: fixed;">';
-
-        /* Make header its own table */
-        if (this.options.fixedHeader) {
-            html = '<table class="supergrid supergrid-fheader">' + headerHtml + '</table>'
-                + '<div class="supergrid-body">' + html;
-        } else {
-            html += headerHtml;
-        }
-
-        html += '<tbody>';
-        html += bodyHtml;
-        html += '</tbody>';
-        html += (this.options.fixedHeader) ? '</table></div>' : '</table>';
-        return html;
+    _buildGrid: function() {
+        this.options._grid.push('<div class="supergrid">');
+        this._buildHeader();
+        this._buildBody();
+        this.options._grid.push('</div>');
     },
 
-    _buildHeader: function () {
-        var headerHtml = '<thead class="supergrid_header">';
-        headerHtml += '<tr>';
-        $.each(this.options.columns, function (i, col) {
+    _buildHeader: function() {
+        if (this.options.fixedHeader)
+            this.options._grid.push('<div class="supergrid-fheader">');
+
+        this.options._grid.push(' <div class="supergrid_row supergrid_header">');
+
+        $.each(this.options.columns, function(i, col) {
             var cellClass = col.cellClass || '',
                 width = col.width || '',
                 id = col.id || '',
                 name = col.name || '',
                 sort = col.sort || '',
-                sortable = col.sortable || '';
+                sortable = col.sortable || '',
+                cellStr = '';
+            cellStr += '<div style="width:' + col.width + 'px;" scope="col" class="supergrid_cell ' + cellClass + '" data-id="' + id + '" tabIndex="0" ';
+            if (sort)
+                cellStr += 'data-sort="' + sort + '" ';
+            cellStr += 'data-sortable="' + sortable + '">';
+            cellStr += '<div>';
+            cellStr += name;
+            if (sort)
+                cellStr += '<div class="sort-icon"></div>';
+            cellStr += '</div>';
+            cellStr += '</div>';
+            this.options._grid.push(cellStr);
+        }.bind(this));
 
-            headerHtml += '<td style="width:' + col.width + 'px;" scope="col" class="' + cellClass + '" data-id="' + id + '" tabIndex="0"';
-            if (sort) {
-                headerHtml += 'data-sort="' + sort + '" ';
-            }
-            headerHtml += 'data-sortable="' + sortable + '">';
-            headerHtml += '<div>';
-            headerHtml += name;
-            if (sort) {
-                headerHtml += '<div class="sort-icon"></div>';
-            }
-            headerHtml += '</div>';
-            headerHtml += '</td>';
-        });
-        headerHtml += '</tr>';
-        headerHtml += '</thead>';
-        return headerHtml;
+        this.options._grid.push(' </div>');
+
+        if (this.options.fixedHeader)
+            this.options._grid.push('</div>');
+
     },
 
-    _buildBody: function () {
+    _buildBody: function() {
         var data = this.options.data,
             columns = this.options.columns,
             context = this,
             bodyHtml = '';
-        $.each(data, function (i, dataSet) {
-            var id = dataSet.id || '';
+        if (this.options.fixedHeader)
+            this.options._grid.push('<div class="supergrid supergrid_body">');
 
-            bodyHtml += '<tr class="section" data-id="' + id + '">';
-            $.each(columns, function (i, col) {
+        $.each(data, buildRow);
+
+        if (this.options.fixedHeader)
+            this.options._grid.push('</div>');
+
+        function buildRow(i, dataSet){
+            var id = dataSet.id || '',
+                row = '';
+            row += '<div class="supergrid_row section" data-id="' + id + '">';
+            $.each(columns, function(i, col) {
                 var cellClass = col.cellClass || '';
-                bodyHtml += '<td style="width:' + col.width + 'px; class="' + cellClass + '" tabIndex="0">';
-                bodyHtml += '<div>';
-                bodyHtml += context._buildCell(dataSet, col);
-                bodyHtml += '</div>';
-                bodyHtml += '</td>';
+                row += '<div style="width:' + col.width + 'px;" class="supergrid_cell ' + cellClass + '" tabIndex="0">';
+                row += '<div>';
+                row += buildCell(dataSet, col);
+                row += '</div>';
+                row += '</div>';
             });
-            bodyHtml += '</tr>';
-        });
-        return bodyHtml;
-    },
-
-    _buildCell: function (data, column) {
-        var attrs = [],
-            regex = /\#(.*?)\#/,
-            formatter = column.formatter,
-            formatterHelper = formatter,
-            matchedAttr = regex.exec(formatter);
-        if (!data) {
-            return '';
+            row += '</div>';
+            context.options._grid.push(row);
         }
-        if (typeof formatter === 'function') {
-            return formatter(data);
-        }
-        if (typeof formatter === 'object') {
-            formatter = column.formatter;
-        }
-
-        formatterHelper = formatter;
-        matchedAttr = regex.exec(formatter);
-        if (formatterHelper) {
-            while (matchedAttr) {
-                attrs.push(matchedAttr[0]);
-                formatterHelper = formatterHelper.replace(matchedAttr[0], '');
-                matchedAttr = regex.exec(formatterHelper);
+        function buildCell(data, column) {
+            var attrs = [],
+                regex = /\#(.*?)\#/,
+                formatter = column.formatter,
+                formatterHelper = formatter,
+                matchedAttr = regex.exec(formatter),
+                context = this;
+            if (!data) {
+                return '';
             }
-            $.each(attrs, function (index, attr) {
-                var value = attr.replace(/#|_/g, '');
-                formatter = formatter.replace(attr, data[value]);
-            });
-            return formatter;
-        } else {
-            return data[column.id];
+            if (typeof formatter === 'function') {
+                return formatter(data);
+            }
+            if (typeof formatter === 'object') {
+                formatter = column.formatter;
+            }
+
+            formatterHelper = formatter;
+            matchedAttr = regex.exec(formatter);
+            if (formatterHelper) {
+                while (matchedAttr) {
+                    attrs.push(matchedAttr[0]);
+                    formatterHelper = formatterHelper.replace(matchedAttr[0], '');
+                    matchedAttr = regex.exec(formatterHelper);
+                }
+                $.each(attrs, function(index, attr) {
+                    var value = attr.replace(/#|_/g, '');
+                    formatter = formatter.replace(attr, data[value]);
+                });
+               return formatter;
+            } else {
+                return data[column.id];
+            }
         }
     },
 
     /**
-     * @memberOf SuperGrid
+     * @name custom.SuperGrid#updateGrid
      * @description Updates the table with new data and optionally new column configs.
-     * @param data(required)
-     * @param columns
+     * @function
+     * @param {Array} data New Data to push to the grid.
+     * @param {Array} columns (optional)
+     * @fires SuperGrid#_renderGrid
      */
-    updateGrid: function (data, columns) {
+    updateGrid: function(data, columns) {
         data && (this.options.data = $.extend([], data));
         columns && (this.options.columns = $.extend([], columns));
         this.element.empty();
         this._renderGrid();
     }
-})
-;
+});
