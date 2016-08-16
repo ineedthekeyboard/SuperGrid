300f43c04ecb538250a52289f6add74cc79771a0
Ryan Carlton <ryan.lee.carlton@gmail.com>
2016-08-16 09:05:59 -0400

    Gulp Build Updates: 	* Sample HTML for Dist added 	* Build for libraries included file added 	* Build for std dist files added 	* Build for CSS updated/added 	* Complete build now possible with gulp buildall

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ var webserver = require('gulp-webserver');
 var jsdoc = require('gulp-jsdoc3');
 var concat = require('gulp-concat');
 var  uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 
-gulp.task('serve',['docs'], function () {
+gulp.task('serve', ['docs'], function () {
     gulp.src('app')
         .pipe(webserver({
             fallback: 'index.html',
@@ -23,13 +24,45 @@ gulp.task('docs', function (cb) {
 });
 
 //Build Simple JS File for external use:
-var files = [
-  'app/js/supergrid/**/*.js'
+var fullDistCSS = [
+  'app/css/normalize.css',
+  'app/css/supergrid.css'
+];
+var fullDist = [
+  'app/js/vendor/jquery-3.1.0.js',
+  'app/js/vendor/jquery-ui-1.12.0-core.js',
+  'app/js/supergrid/supergrid.js'
 ];
 
-gulp.task('build', function() {
-  return gulp.src(files)
+//Build a dist with libraries included
+gulp.task('buildCSSFullDist', function(){
+  return gulp.src(fullDistCSS)
+    .pipe(cssmin())
+    .pipe(concat('supergrid.min.css'))
+    .pipe(gulp.dest('dist/full'));
+});
+gulp.task('buildFullDist', ['buildCSSFullDist'], function(){
+  return gulp.src(fullDist)
+    .pipe(uglify())
+    .pipe(concat('supergrid.min.js'))
+    .pipe(gulp.dest('dist/full'));
+});
+
+//Standard Supergrid Only Build
+gulp.task('buildCSS', function(){
+  return gulp.src('app/css/supergrid.css')
+    .pipe(cssmin())
+    .pipe(concat('supergrid.min.css'))
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('buildSample', function(){
+  return gulp.src('app/sample.html')
+    .pipe(gulp.dest('dist'));
+});
+gulp.task('build', ['buildCSS', 'buildSample'], function() {
+  return gulp.src(['app/js/supergrid/supergrid.js'])
     .pipe(uglify())
     .pipe(concat('supergrid.min.js'))
     .pipe(gulp.dest('dist'));
 });
+gulp.task('buildall',['build', 'buildFullDist', 'docs']);
